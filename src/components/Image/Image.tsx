8ac6a36354bc1d0e2fcbc118fd1f6bd3309fe58e
kienvi <kienvi96@hotmail.com>
2022-12-06 16:43:06 +1000

    Updated image component to have a fallback incase an image cannot be found in assets.

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -5,14 +5,21 @@ import { useEffect, useState } from "react";
 
 export default function Image({ id, optionDetails, onImageClick }: { id: string, optionDetails: ImageInterface, onImageClick: Function }) {
 
-    // Keeps track of when the entire comparer is loading to prevent either buttons from clicking.
+    // Keeps track of when the entire comparer is loading to prevent either buttons from clicking
     // Variable selected is used to track internally which image should be showing the loading transition
     const isLoading = useImageSelectionStore(state => state.isLoading);
-    const [selected, setSelected] = useState(false);  
+    const [selected, setSelected] = useState(false);
+    // Set it as true on the assumption it can be found, and turn off if it can't be
+    const [imageIsFound, setImageIsFound] = useState(true);  
 
     useEffect(() => {
         if (isLoading === false) setSelected(false);
     }, [isLoading])
+
+    // Ensure the component will always be given the chance to find the image
+    useEffect(() => {
+        setImageIsFound(true);
+    }, [optionDetails])
 
     const getClassName = (): string => {
         return `image-container ${ selected ? "fade" : "" }`;
@@ -26,7 +33,16 @@ export default function Image({ id, optionDetails, onImageClick }: { id: string,
         }
     }
 
+    const onImageNotFound = () => {
+        setImageIsFound(false);
+    }
+
     return (
-        <img className={getClassName()} src={optionDetails.path} alt={optionDetails.alt} onError={() => console.log("error")} onClick={onImageClickChild}/>
+        <div className={getClassName()} onClick={onImageClickChild}>
+            { imageIsFound ? 
+                <img src={optionDetails.path} alt={optionDetails.alt} onError={() => onImageNotFound()} /> :
+                <div>Can't find img</div>
+            }       
+        </div>
     )
 }
